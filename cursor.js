90b0a5dd1207e76c3644b6846159416bf0141fed
Ryan Harricharan <138528052+rharricharan@users.noreply.github.com>
2025-02-10 16:04:29 -0500

    improving cursor animations

--- cursor.js
+++ cursor.js
@@ -2,6 +2,10 @@ document.addEventListener("DOMContentLoaded", () => {
     const cursor = document.querySelector(".custom-cursor");
     const cursorText = document.querySelector(".cursor-text");
     const projects = document.querySelectorAll(".project");
+    const homeItem = document.querySelectorAll(".home-item");
+    const workItem = document.querySelectorAll(".work-item");
+    const expItem = document.querySelectorAll(".exp-item");
+    const contactItem = document.querySelectorAll(".contact-item")
 
     function isTouchDevice() {
         return window.innerWidth <= 1024 || "ontouchstart" in window;
@@ -15,8 +19,8 @@ document.addEventListener("DOMContentLoaded", () => {
     // Mouse move effect
     document.addEventListener("mousemove", (e) => {
         gsap.to(cursor, {
-            x: e.clientX - cursor.clientWidth / 2,
-            y: e.clientY - cursor.clientHeight / 2,
+            x: e.clientX - 40,
+            y: e.clientY - 40,
             duration: 0.2,
             ease: "power2.out",
             opacity: 1
@@ -34,15 +38,15 @@ document.addEventListener("DOMContentLoaded", () => {
         gsap.to(cursorText, {
             y: -10,
             opacity: 0,
-            duration: 0.3,
-            ease: "power2.Inout",
+            duration: 0.2,
+            ease: "power2.out",
             onComplete: () => {
                 cursorText.textContent = newText;
 
                 // Animate in the new text
                 gsap.fromTo(cursorText, 
                     { y: 10, opacity: 0 }, 
-                    { y: 0, opacity: 1, duration: 0.3, ease: "power2.Inout" }
+                    { y: 0, opacity: 1, duration: 0.2, ease: "power2.out" }
                 );
             }
         });
@@ -52,12 +56,69 @@ document.addEventListener("DOMContentLoaded", () => {
     projects.forEach((project) => {
         project.addEventListener("mouseenter", () => {
             updateCursorText(project.getAttribute("data-text"));
-            gsap.to(cursor, { scale: 1.1, backgroundColor: "rgba(255, 255, 255, 1)" });
+            gsap.to(cursor, { scale: 1.12, backgroundColor: "rgba(255, 255, 255, 1)" });
         });
 
         project.addEventListener("mouseleave", () => {
             updateCursorText("(scroll)");
             gsap.to(cursor, { scale: 1, backgroundColor: "rgba(255, 255, 255, 0.5)" });
+            gsap.to(cursor.querySelector(".cursor-text"), { color: "rgba(0, 0, 0, 1)" });
+        });
+    });
+
+    homeItem.forEach((homeItem) => {
+        homeItem.addEventListener("mouseenter", () => {
+            updateCursorText(homeItem.getAttribute("data-text"));
+            gsap.to(cursor, { scale: 1.12, backgroundColor: "#0898FB" });
+            gsap.to(cursor.querySelector(".cursor-text"), { color: "rgba(255, 255, 255, 1)" });
+        });
+
+        homeItem.addEventListener("mouseleave", () => {
+            updateCursorText("(scroll)");
+            gsap.to(cursor, { scale: 1, backgroundColor: "rgba(255, 255, 255, 0.5)" });
+            gsap.to(cursor.querySelector(".cursor-text"), { color: "rgba(0, 0, 0, 1)" });
+        });
+    });
+
+    workItem.forEach((workItem) => {
+        workItem.addEventListener("mouseenter", () => {
+            updateCursorText(workItem.getAttribute("data-text"));
+            gsap.to(cursor, { scale: 1.12, backgroundColor: "#FF4E12" });
+            gsap.to(cursor.querySelector(".cursor-text"), { color: "rgba(255, 255, 255, 1)" });
+        });
+
+        workItem.addEventListener("mouseleave", () => {
+            updateCursorText("(scroll)");
+            gsap.to(cursor, { scale: 1, backgroundColor: "rgba(255, 255, 255, 0.5)" });
+            gsap.to(cursor.querySelector(".cursor-text"), { color: "rgba(0, 0, 0, 1)" });
+        });
+    });
+
+    expItem.forEach((expItem) => {
+        expItem.addEventListener("mouseenter", () => {
+            updateCursorText(expItem.getAttribute("data-text"));
+            gsap.to(cursor, { scale: 1.12, backgroundColor: "#DC1DA9" });
+            gsap.to(cursor.querySelector(".cursor-text"), { color: "rgba(255, 255, 255, 1)" });
+        });
+
+        expItem.addEventListener("mouseleave", () => {
+            updateCursorText("(scroll)");
+            gsap.to(cursor, { scale: 1, backgroundColor: "rgba(255, 255, 255, 0.5)" });
+            gsap.to(cursor.querySelector(".cursor-text"), { color: "rgba(0, 0, 0, 1)" });
+        });
+    });
+
+    contactItem.forEach((contactItem) => {
+        contactItem.addEventListener("mouseenter", () => {
+            updateCursorText(contactItem.getAttribute("data-text"));
+            gsap.to(cursor, { scale: 1.12, backgroundColor: "#11D238" });
+            gsap.to(cursor.querySelector(".cursor-text"), { color: "rgba(255, 255, 255, 1)" });
+        });
+
+        contactItem.addEventListener("mouseleave", () => {
+            updateCursorText("(scroll)");
+            gsap.to(cursor, { scale: 1, backgroundColor: "rgba(255, 255, 255, 0.5)" });
+            gsap.to(cursor.querySelector(".cursor-text"), { color: "rgba(0, 0, 0, 1)" });
         });
     });
 });
