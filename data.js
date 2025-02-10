ceed3a86603fb392d3aef20be04379af152cce9e
Ryan Harricharan <138528052+rharricharan@users.noreply.github.com>
2025-02-10 14:54:52 -0500

    pushing new portfolio

---
+++ data.js
@@ -0,0 +1,44 @@
+export const data = [
+  {
+    title: "ryan harricharan",
+    tagline: "3x award winning",
+    year: "ux/product designer",
+    tag: "(scroll)",
+    link: "https://nebulaquest.io",
+  },
+  {
+    title: "smartstop",
+    tagline: "0 -> 1 product design",
+    year: "2024",
+    tag: "product design",
+    link: "https://aurorascribe.com",
+  },
+  {
+    title: "our next energy",
+    tagline: "setting company standards for growth and retention",
+    year: "2023",
+    tag: "sr. ux designer",
+    link: "https://echocircuit.net",
+  },
+  {
+    title: "ore®",
+    tagline: "a different approach to ticketing",
+    year: "(aquired by TicketMaster)",
+    tag: "co-founder & lead product designer",
+    link: "https://zenithhorizon.com",
+  },
+  {
+    title: "catchrays",
+    tagline: "mobile tanning app enhanced for safety",
+    year: "2023",
+    tag: "product designer",
+    link: "https://prismarchitect.dev",
+  },
+  {
+    title: "halftone",
+    tagline: "a fresh presence to music and manufacturing",
+    year: "2022",
+    tag: "web design/development",
+    link: "https://prismarchitect.dev",
+  },
+];
